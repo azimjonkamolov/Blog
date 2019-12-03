cbcdceeec1ced4466e01e64c8ec1709799ed96fe
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-04 03:23:38 +0900

    home page to dashboard page

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,5 @@ Route::resource('posts', 'PostsController');
 
 
 
-
-
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index');
