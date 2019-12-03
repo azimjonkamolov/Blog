095870b6009e1a3662420bdebdd6ecfde377062f
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-03 15:57:31 +0900

    Create Post is done, but without editor yet

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,5 +2,15 @@
 
 @section('content')
     <h1>Create Post</h1>
+    {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
+        <div class="form-group">
+            {{Form::label('title', 'Title')}}
+            {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+        </div>
+        <div class="form-group">
+                {{Form::label('body', 'Body')}}
+                {{Form::textarea('body', '', ['class' => 'form-control', 'placeholder' => 'Body Text'])}}
+            </div>
+    {!! Form::close() !!}
 
 @endsection
