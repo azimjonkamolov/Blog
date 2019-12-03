b7fd701c4bde2de883de2bea64704669c1a1a4a6
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-03 14:03:57 +0900

    app file in layouts created less html repeat

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'LSAPP')}}</title>
+
+
+    </head>
+    <body>
+       
+        @yield('content')
+
+    </body>
+</html>
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,18 +1,10 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{config('app.name', 'LSAPP')}}</title>
-
-
-    </head>
-    <body>
-       
+@extends('layouts.app')
+    @section('content')
         <h1>Services</h1>
-        <p>This is the services</p>
+        <p>This is Services</p>
+    @endsection
 
-    </body>
-</html>
+
+
+
+    
