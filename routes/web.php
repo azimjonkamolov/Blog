133bb073de878318d722f6a0cfcc18c6fe5b7e4c
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-03 15:05:39 +0900

    PostsController, database connection.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
+
+Route::resource('post', 'PostsController');
+
+
+
+
+
+
