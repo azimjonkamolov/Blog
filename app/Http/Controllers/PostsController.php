170d0118c44154a896863f48fbc44ef314ffbc95
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-05 16:14:22 +0900

    Access control user request before createing a post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,16 @@ use DB;
 
 class PostsController extends Controller
 {
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,7 +10,6 @@
     <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
     <hr>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
-
     {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
         {{Form::hidden('_method', 'DELETE')}}
         {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
