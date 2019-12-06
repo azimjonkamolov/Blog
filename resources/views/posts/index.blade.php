950118b4e179eee5bfc1d1a497319484c8f90ff4
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-06 14:26:53 +0900

    image size fix

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,10 +6,10 @@
         @foreach ($posts as $post)
             <div class="well">
                 <div class="row">
-                    <div class="col-md-4 col-sm-4">
+                    <div class="col-md-2 col-sm-2">
                         <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}">
                     </div> 
-                    <div class="col-md-8 col-sm-8">
+                    <div class="col-md-10 col-sm-10">
                         <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
                         <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
                     </div>
