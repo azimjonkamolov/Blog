1cdde81dce86a5ce8941bad76603298815e4573b
Azimjon Kamolov <azimjon.6561@gmail.com>
2020-02-09 21:58:16 +0900

    Cleaned the code

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,6 @@
         <div class="jumbotron text-center">
             <h1>Welcome To Akloop Blogs!</h1>
             <p>This is the laravel application to share ideas</p>
-            {{-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Login</a> <a class="btn btn-success btn-lg" href="#" role="button">Register</a></p> --}}
         </div>
     @endsection
 
